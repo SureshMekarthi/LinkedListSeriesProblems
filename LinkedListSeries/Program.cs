7ab1d684949ce70d80643ca91f68ddcb55b915e2
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-07-16 20:30:07 -0400

    insert at  head

--- LinkedListSeries/Program.cs
+++ LinkedListSeries/Program.cs
@@ -10,6 +10,22 @@ namespace LinkedListSeries
     {
         static void Main(string[] args)
         {
+            List<int> arr = new List<int> { 12, 8, 5, 7 };
+            int val = 100;
+
+            // Creating a linked list with initial elements from the array
+            Node head = new Node(arr[0]);
+            head.next = new Node(arr[1]);
+            head.next.next = new Node(arr[2]);
+            head.next.next.next = new Node(arr[3]);
+
+            // Inserting a new node at the head of the linked list
+            head = Node.InsertHead(head, val);
+
+            // Printing the linked list
+            Node.PrintLL(head);
+
+            Console.ReadLine();
         }
     }
 }
