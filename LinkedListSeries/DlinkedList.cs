a8bbc58fd25e622ff074066ab18aac369bba8008
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-07-30 19:18:52 -0400

    added NodeD class

---
+++ LinkedListSeries/DlinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListSeries
+{
+    public class DlinkedList
+    {
+        public static class Node
+        {
+            public int data;
+            public Node next;
+            public Node back;
+
+            public Node()
+            {
+                
+            }
+
+        }
+    }
+}
---
+++ LinkedListSeries/NodeD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListSeries
+{
+    public class NodeD
+    {
+        public int data;
+        public NodeD next;
+        public NodeD back;
+
+        public NodeD(int data, NodeD next, NodeD back)
+        {
+            this.data = data;
+            this.next = next;
+            this.back = back;
+        }
+        public NodeD(int data)
+        {
+            this.data = data;
+            next = null;
+            back = null;
+        }
+
+    }
+}
