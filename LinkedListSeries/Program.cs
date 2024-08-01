87d0164c977654d8d25f1e767a6698b4b33d9922
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-08-01 11:37:10 -0400

    updated middle node calss

--- LinkedListSeries/Program.cs
+++ LinkedListSeries/Program.cs
@@ -10,14 +10,14 @@ namespace LinkedListSeries
     {
         static void Main(string[] args)
         {
-            //List<int> arr = new List<int> { 12, 8, 5, 7 };
+            List<int> arr = new List<int> { 12, 8, 5, 7 };
             //int val = 100;
 
             //// Creating a linked list with initial elements from the array
-            //Node head = new Node(arr[0]);
-            //head.next = new Node(arr[1]);
-            //head.next.next = new Node(arr[2]);
-            //head.next.next.next = new Node(arr[3]);
+            Node head = new Node(arr[0]);
+            head.next = new Node(arr[1]);
+            head.next.next = new Node(arr[2]);
+            head.next.next.next = new Node(arr[3]);
 
             //// Inserting a new node at the head of the linked list
             //head = Node.InsertHead(head, val);
@@ -25,12 +25,16 @@ namespace LinkedListSeries
             //// Printing the linked list
             //Node.PrintLL(head);
 
-            int[] array = { 12, 5, 6, 8 };
-            NodeD head = NodeD.ConvertArr2DLL(array);
-            NodeD.Print(head);
+            //int[] array = { 12, 5, 6, 8 };
+            //NodeD head = NodeD.ConvertArr2DLL(array);
+            //NodeD.Print(head);
 
-            head = NodeD.DeleteHead(head);
-            NodeD.Print(head);
+           Node middleNode = FindMiddleOfLinkedList.FindMiddle(head);
+
+            Console.WriteLine($" then middle node is: {middleNode.data}");
+
+            //head = NodeD.DeleteHead(head);
+            //NodeD.Print(head);
             Console.ReadLine();
         }
     }
