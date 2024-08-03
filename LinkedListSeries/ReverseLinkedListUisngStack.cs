773fd4b0408b7b005e8e54aea598a9674b851e9d
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-08-02 20:32:58 -0400

    added ReverseLinkedListUisngStack

---
+++ LinkedListSeries/ReverseLinkedListUisngStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListSeries
+{
+    public class ReverseLinkedListUisngStack
+    {
+        public static Node ReverseLinkedList(Node head)
+        {
+            Node temp = head;
+
+            Stack<int> st = new Stack<int>();
+
+            while (temp != null)
+            {
+                st.Push(temp.data);
+                temp = temp.next;
+            }
+
+            temp = head;
+
+            while (temp != null) { 
+                temp.data = st.Pop();
+                temp = temp.next;
+            }
+            return head;
+        }
+    }
+}
