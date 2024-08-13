26a2e35dd342c6e68cc5b20babd0bddf32866de5
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-08-13 19:16:01 -0400

    added reorder list

---
+++ LinkedListSeries/ReOrderList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListSeries
+{
+    public class ReOrderList
+    {
+        public static Node Middle(Node head)
+        {
+            Node slow = head;
+            Node fast = head;
+
+            while (fast.next != null && fast.next.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+
+        public static Node Reverse(Node head)
+        {
+            Node temp = head;
+            Node prev = null;
+            Node fort = null;
+
+            while (temp != null)
+            {
+                fort = temp.next;
+                temp.next = prev;
+                prev = temp;
+                temp = temp.next;
+            }
+            return prev;
+        }
+    }
+}
